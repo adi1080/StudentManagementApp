239e9253968cadab2eae6b7a483509e9b2bddb65
adi1080 <adityaborsarkar@gmail.com>
2025-05-16 18:38:07 +0530

    Forking

--- SMSAPP-bootstrap/src/main/java/edu/smsapp/app/SmsappBootstrapApplication.java
+++ SMSAPP-bootstrap/src/main/java/edu/smsapp/app/SmsappBootstrapApplication.java
@@ -12,7 +12,7 @@ public class SmsappBootstrapApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmsappBootstrapApplication.class, args);
-		System.out.println("");
+		System.out.println("testing forking");
 	}
 
 }
